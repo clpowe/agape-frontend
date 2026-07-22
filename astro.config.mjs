3010ee779e8fd1e1ed21c6d6ef4609f9f85e0ae0
clpowe <clpowe@gmail.com>
2026-07-21 22:33:44 -0400

    Added navigation bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,30 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import { defineConfig, fontProviders } from "astro/config";
+import Icons from "unplugin-icons/vite";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
+
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Playfair Display",
+      cssVariable: "--font-playfair",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Noto Sans",
+      cssVariable: "--font-noto",
+    },
+  ],
+
+  vite: {
+    plugins: [
+      Icons({
+        compiler: "astro",
+      }),
+    ],
+  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite-plus";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
   staged: {
     "*": "vp check --fix",
   },
   fmt: {},
+  plugins: [
+    Icons({
+      compiler: "astro",
+    }),
+  ],
   lint: {
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
     rules: { "vite-plus/prefer-vite-plus-imports": "error" },
