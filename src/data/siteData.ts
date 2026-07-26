953802ee4571bce7bc65faab2c029c70055fe528
clpowe <clpowe@gmail.com>
2026-07-25 22:07:15 -0400

    Added Base Head component and started About pages

---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+  title: "Agape Christian Bar Prep",
+  description:
+    "Agape Christian Bar Prep helps students prepare to pass the bar exam through personalized services that offer a holistic approach to bar exam preparation.",
+};
