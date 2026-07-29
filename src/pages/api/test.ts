aff68d788b32e6ec75b569db68edfb563d7efdc7
clpowe <clpowe@gmail.com>
2026-07-29 06:39:14 -0400

    test api

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,6 +1,6 @@
 export const prerender = false;
 
-export async function POST() {
+export async function GET() {
   return new Response(JSON.stringify({ ok: true }), {
     headers: { "Content-Type": "application/json" },
   });
